bf6b623922e382544e4e25744abc9b4350d45a41
Harsha2122 <tsingh2214@gmail.com>
2023-10-25 01:14:28 +0530

    hasbxb

--- app/page.js
+++ app/page.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import '/style.css'
+import '../style.css'
+
 
 const page = () => {
   return (
     <>
-      <div className="dashboard">
+    <div className="dashboard">
     <div className="overlap-group-wrapper">
 
         <div className="headlines" />
