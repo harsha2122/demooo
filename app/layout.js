baafaf8a4e9dbcbce363048cc62cb9cfcdf1850e
Harsha2122 <tsingh2214@gmail.com>
2023-10-25 15:29:55 +0530

    asjbjkas

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,11 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
-import '../style.css'
+"use client"
+import './globals.css';
+import { Inter } from 'next/font/google';
+import '../style.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'Demo',
-  description: 'React Next App',
-}
+
 
 export default function RootLayout({ children }) {
   return (
@@ -19,86 +17,80 @@ export default function RootLayout({ children }) {
           <div class="component">
             <div class="overlap">
               <div class="rectangle"></div>
-              <div class="group">
-                <div class="div">
-                  <img class="img" src="https://c.animaapp.com/DC1Sd5sj/img/vuesax-outline-category-2.svg" />
-                  <a href="/Pages/Dashboard" class="text-wrapper">Dashboard</a>
-                </div>
-                <div class="group-2">
-                  <div class="frame-wrapper">
-                    <div class="frame">
-                      <img class="img-2" src="https://c.animaapp.com/DC1Sd5sj/img/iconly-6.svg" />
-                      <a href="/Pages/Business" class="text-wrapper-2">Businesses</a>
-                      <img
-                        class="vuesax-outline-arrow"
-                        src="https://c.animaapp.com/DC1Sd5sj/img/vuesax-outline-arrow-down-3.svg"
-                      />
-                    </div>
-                  </div>
-                  <div class="frame-2">
-                    <a class="cafe-restaurant">Cafe &amp; Restaurant</a>
-                    <a class="text-wrapper-3">Shopping</a>
-                    <a class="building">Building &amp; Construction</a>
-                    <a class="text-wrapper-3">Resourses</a>
-                    <a class="text-wrapper-3">Opportunities</a>
-                    <a class="text-wrapper-4">Rewards</a>
-                  </div>
-                  
-                </div>
-                <div class="div-wrapper">
-                  <div class="frame-3">
-                    <img class="vector" src="https://c.animaapp.com/DC1Sd5sj/img/vector-9-2.svg" />
-                    <div class="group-wrapper">
-                      <div class="group-3">
-                        <div class="frame-4">
-                          <img class="img-2" src="https://c.animaapp.com/DC1Sd5sj/img/iconly-7.svg" />
-                          <div class="text-wrapper-5">Events</div>
+              <ul class="group">
+                  <li class="div hh">
+                    <img class="img" src="https://c.animaapp.com/DC1Sd5sj/img/vuesax-outline-category-2.svg" />
+                    <a href="/Pages/Dashboard" class="text-wrapper">Dashboard</a>
+                  </li>
+                  <li class="group-2 dropdown hh">
+                      <ul class="frame-wrapper">
+                          <div class="frame-4">
+                              <img class="img-2" src="https://c.animaapp.com/DC1Sd5sj/img/iconly-6.svg" />
+                              <a class="text-wrapper-2">Businesses</a>
+                              <img class="vuesax-outline-arrow" src="https://c.animaapp.com/DC1Sd5sj/img/vuesax-outline-arrow-down-3.svg" />
+                          </div>
+                      </ul>
+                      <ul class="frame-2 dropdown-content3">
+                          <li><a href="/Pages/Business" class="cafe-restaurant">Cafe &amp; Restaurant</a></li>
+                          <li><a href='#' class="text-wrapper-3">Shopping</a></li>
+                          <li><a href="#" class="building">Building &amp; Construction</a></li>
+                          <li><a href="#" class="text-wrapper-3">Resourses</a></li>
+                      </ul>
+                  </li>
+                  <li class="div-wrapper dropdown hh">
+                    <li class="frame-3">
+                      <img class="vector" src="https://c.animaapp.com/DC1Sd5sj/img/vector-9-2.svg" />
+                      <div class="group-wrapper">
+                        <div class="group-3">
+                          <div class="frame-4">
+                            <img class="img-2" src="https://c.animaapp.com/DC1Sd5sj/img/iconly-7.svg" />
+                            <div class="text-wrapper-5">Events</div>
+                            <img class="vuesax-outline-arrow1" src="https://c.animaapp.com/DC1Sd5sj/img/vuesax-outline-arrow-down-3.svg" />
+                          </div>
                         </div>
                       </div>
-                    </div>
-                  </div>
-                </div>
+                    </li><ul class="frame-2 dropdown-content4">
+                          <li><a href='/Pages/Uevent' class="cafe-restaurant">Upcoming Events</a></li>
+                          <li><a href='/Pages/Eventsingle' class="text-wrapper-3">Events Booked</a></li>
+                      </ul>
+                </li>
                 <img class="vector-2" src="https://c.animaapp.com/DC1Sd5sj/img/vector-10-1.svg" />
                 <img class="vector-3" src="https://c.animaapp.com/DC1Sd5sj/img/vector-10-1.svg" />
-                <div class="group-4">
+                <li class="group-4 hh">
                   <div class="group-5">
                     <div class="frame-4">
                       <img class="iconly" src="https://c.animaapp.com/DC1Sd5sj/img/iconly-8.svg" />
                       <a href="/Pages/News" class="text-wrapper-5">News</a>
                     </div>
                   </div>
-                </div>
+                </li>
                 <img class="vector-4" src="https://c.animaapp.com/DC1Sd5sj/img/vector-12-1.svg" />
-                <div class="group-6">
+                <li class="group-6 hh">
                   <div class="group-7">
                     <div class="frame-4">
                       <img class="iconly-2" src="https://c.animaapp.com/DC1Sd5sj/img/iconly-9.svg" />
                       <a href="#" class="text-wrapper-5">Resources</a>
                     </div>
                   </div>
-                </div>
+                </li>
                 <img class="vector-5" src="https://c.animaapp.com/DC1Sd5sj/img/vector-13-1.svg" />
-                <div class="group-8">
+                <li class="group-8 hh">
                   <div class="group-9">
                     <div class="frame-4">
                       <img class="img-2" src="https://c.animaapp.com/DC1Sd5sj/img/iconly-10.svg" />
                       <a href="/Pages/Oppurtunities" class="text-wrapper-5">Opportunities</a>
                     </div>
                   </div>
-                </div>
+                </li>
                 <img class="vector-6" src="https://c.animaapp.com/DC1Sd5sj/img/vector-11-1.svg" />
-                <div class="group-10">
+                <li class="group-10 hh">
                   <div class="frame-5">
                     <img class="iconly-3" src="https://c.animaapp.com/DC1Sd5sj/img/iconly-11.svg" />
                     <a href="/Pages/Rewwardss" class="text-wrapper-6">Rewards</a>
                   </div>
-                </div>
-                <img class="vector-7" src="https://c.animaapp.com/DC1Sd5sj/img/vector-8-1.svg" />
-            
-                
-                <a href="/Pages/Uevent" class="text-wrapper-7">Upcoming Events</a>
-                <a href="/Pages/Eventsingle" class="text-wrapper-8">Event Booked</a>
-              </div>
+                </li>
+
+              </ul>
 
               <div class="rectangle-2"></div>
               <div class="group-12">
@@ -163,7 +155,7 @@ export default function RootLayout({ children }) {
           </div>
           
           </div>
-            
+
       </body>
     </html>
   )
